d5b4e0def74ac715fd3bc91408881c7de42dcb52
Philip Heimböck <philipheimboeck@gmail.com>
2016-05-21 13:18:19 +0200

    Added multiple options to answer

--- src/python/modules/hello.py
+++ src/python/modules/hello.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
-import re, sys, os
+import sys, os
+import random, re
 
 # Import required modules
 sys.path.append(os.environ['PLANTBOT_HOME'] + "/src/python/")
 from twitter import *
 
-WORDS = ["HELLO", "WELCOME"]
+WORDS = ["HELLO", "WELCOME", "GREETINGS"]
 
 def isValid(text):
-    return bool(re.search(r'\bhello\b', text, re.IGNORECASE)) or  bool(re.search(r'\bwelcome\b', text, re.IGNORECASE))
+    return bool(re.search(r'((\bhello|greetings|welcome\b))', text, re.IGNORECASE))
 
 def handle(text, mic, profile):
 
-    message = "Hello " + profile["first_name"] + ", it is nice to be in " + profile["location"] +" today."
+
+    messages = ["Hello %s, it is nice to be in %s today." % (profile["first_name"], profile["location"]),
+                "Greetings %s! You are allowed to spoil me today." % (profile['first_name']),
+                "Hello %s. What is it what you want?" % (profile['first_name']),
+                "I don't feel like talking right now.",
+                "People were always talking to me. But at least I didn't had to answer."]
+
+    message = random.choice(messages);
+
     tweet(message)
 
     mic.say(message)
