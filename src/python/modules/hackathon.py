224cb29a35f28bdc4823a48115a9e99f17a76dcd
natterstefan <stefan@natter.at>
2016-05-21 13:10:42 +0200

    added hackathon.py module

---
+++ src/python/modules/hackathon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import re, sys, os
+
+# Import required modules
+sys.path.append(os.environ['PLANTBOT_HOME'] + "/src/python/")
+from twitter import *
+
+WORDS = ["umahuesla", "hackathon", "uma", "hysla"]
+
+def isValid(text):
+    return bool(re.search(r'\bhackathon\b', text, re.IGNORECASE)) or  bool(re.search(r'\buma\b', text, re.IGNORECASE))
+
+def handle(text, mic, profile):
+
+    message = "You know what " + profile["first_name"] + "? I REALLY LOVE NERDS AND GEEKS!"
+
+    mic.say(message)
