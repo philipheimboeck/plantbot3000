9c3ddd5aa36cb12ff6fa498bc64e8936bc28340d
Philip Heimböck <philipheimboeck@gmail.com>
2016-05-21 13:31:46 +0200

    Added messages to plantbot

--- src/python/modules/plantbot.py
+++ src/python/modules/plantbot.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python
-import re, sys, os
+import re, sys, os, random
 
 # Import required modules
 sys.path.append(os.environ['PLANTBOT_HOME'] + "/src/python/")
 from sensor_reader import SensorReader
 from twitter import *
 
-WORDS = ["HOW", "ARE", "YOU"]
+WORDS = ["HOW", "ARE", "YOU", "DO", "FEEL", "OK"]
 
 reader = SensorReader()
 
+
 def isValid(text):
-    return bool(re.search(r'\bhow are you\b', text, re.IGNORECASE))
+    return bool(re.search(r'\b(how (are you)|(do you feel))|(you ok)\b', text, re.IGNORECASE))
+
 
 def handle(text, mic, profile):
     reader.read()
     light = reader.getLightPercentage()
     moist = reader.getMoistPercentage()
 
-    message = "My moist level is %d percentage and my light level is %d percentage" % (moist, light)
+    # message = "My moist level is %d percentage and my light level is %d percentage" % (moist, light)
+
+    messages = ["You have a green thumb. I feel good.", "I feel fine.", "Today is a good day",
+                "Thank you for keeping me healthy", "I am growing prosper. Thank you."]
+
+    if moist < 20:
+        messages = ["I am quite thirsty", "Either give me water or kill me right now!",
+                    "You must be confusing me with a cactus.", "Please net me right now!"]
+    elif moist < 50:
+        messages = ["I would like some water", "It is not urgent, but could you net me?",
+                    "Please net me."]
+
+    message = random.choice(messages)
 
     tweet(message)
     mic.say(message)
